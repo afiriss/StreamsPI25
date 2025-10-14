336f6de187ab3dcb66c6e7cba5d7840a7b2b8268
Antonio Vitor Nascimento de Souza <20221064010030@ifrn.local>
2025-10-14 20:27:58 -0300

    Alterando todo o main e css

--- app/controllers/Filmes.java
+++ app/controllers/Filmes.java
@@ -1,17 +1,22 @@
 package controllers;
 
 import java.util.List;
-
 import models.Filme;
 import play.mvc.Controller;
 import play.mvc.With;
 
 @With(Seguranca.class)
-public class Filmes extends Controller{
+public class Filmes extends Controller {
 
-	public static void listar() {
-		List<Filme> filmes = Filme.findAll();
-		render(filmes);
-	}
-	
+    public static void listar() {
+        // Busca os filmes de cada gênero separadamente
+        
+        // CORREÇÃO: Trocado '?' por '?1' em todas as consultas
+        List<Filme> filmesRomance = Filme.find("genero = ?1 order by nome", "romance").fetch();
+        List<Filme> filmesComedia = Filme.find("genero = ?1 order by nome", "comedia").fetch();
+        List<Filme> filmesTerror = Filme.find("genero = ?1 order by nome", "terror").fetch();
+        
+        // Envia as três listas para a view
+        render(filmesRomance, filmesComedia, filmesTerror);
+    }
 }
